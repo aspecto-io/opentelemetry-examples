913d28484fe4235e8fb0a869889c97cff712eea6
Yoav Danieli <35626937+ydan287@users.noreply.github.com>
2022-11-09 08:18:35 +0200

    Kubernetes Example (#8)

---
+++ k8s/services/server/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+});
---
+++ k8s/services/server/tracing.ts
@@ -0,0 +1,26 @@
+import { trace } from '@opentelemetry/api';
+import * as opentelemetry from '@opentelemetry/sdk-node';
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+
+const otelColEndpoint = 
+  process.env.OTEL_COL_OTLP_ENDPOINT || 'http://localhost:4318/v1/traces';
+
+console.log(`Exporting to traces to ${otelColEndpoint}`);
+
+const exporter = new OTLPTraceExporter({
+  url: otelColEndpoint,
+});
+
+const sdk = new opentelemetry.NodeSDK({
+  spanProcessor: new SimpleSpanProcessor(exporter),
+  instrumentations: [getNodeAutoInstrumentations()],
+  serviceName: process.env.SERVICE_NAME,
+});
+
+sdk.start();
+let tracer = trace.getTracer('Tracer')
+let span = tracer.startSpan("New Span")
+span.end();
+console.log(`${process.env.SERVICE_NAME}: Start Tracing...`);
