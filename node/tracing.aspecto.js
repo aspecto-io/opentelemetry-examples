9c992a462e01b1bac08b3a7d7edbf780edea7f2f
Motti Bechhofer <mybechhofer@gmail.com>
2023-01-04 15:18:33 +0200

    add aspecto distro example

--- node/tracing.aspecto.js
+++ node/tracing.aspecto.js
@@ -1,44 +1,12 @@
-//OTEL
-const { Resource } = require("@opentelemetry/resources");
-const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
-const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const aspecto = require("@aspecto/opentelemetry");
 const { trace } = require("@opentelemetry/api");
 
-//exporter
-const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
-
-//instrumentations
-const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
-const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
-const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
-const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-
-module.exports = (serviceName) => {
-  const exporter = new OTLPTraceExporter({
-    url: "https://collector.aspecto.io/v1/traces",
-    headers: {
-      Authorization: process.env.ASPECTO_API_KEY,
-    },
+module.exports = (serviceName) => {            
+  aspecto({
+    aspectoAuth: process.env.ASPECTO_API_KEY,
+    serviceName: serviceName,
+    otlpCollectorEndpoint: "http://localhost:8081/v1/trace",
+    writeSystemLogs: true
   });
-
-  const provider = new NodeTracerProvider({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-    }),
-  });
-  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-
-  provider.register();
-
-  registerInstrumentations({
-    instrumentations: [
-      new HttpInstrumentation(),
-      new ExpressInstrumentation(),
-      new MongoDBInstrumentation(),
-    ],
-    tracerProvider: provider,
-  });
-
-  return trace.getTracer(serviceName);
+  return trace.getTracerProvider().getTracer(serviceName);
 };
