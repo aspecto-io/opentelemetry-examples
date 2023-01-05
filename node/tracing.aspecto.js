4d543e4cc63220ef0d7e092b08c0873713cb91c5
Motti Bechhofer <mybechhofer@gmail.com>
2023-01-05 12:27:16 +0200

    remove extra config

--- node/tracing.aspecto.js
+++ node/tracing.aspecto.js
@@ -1,12 +1,10 @@
 const aspecto = require("@aspecto/opentelemetry");
 const { trace } = require("@opentelemetry/api");
 
-module.exports = (serviceName) => {            
+module.exports = (serviceName) => {
   aspecto({
     aspectoAuth: process.env.ASPECTO_API_KEY,
     serviceName: serviceName,
-    otlpCollectorEndpoint: "http://localhost:8081/v1/trace",
-    writeSystemLogs: true
   });
   return trace.getTracerProvider().getTracer(serviceName);
 };
